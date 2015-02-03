6ad5965f9a693a98fe1e00f0119333e828f3537e
Daniel Flax <dflax@yahoo.com>
2015-02-02 21:22:12 -0500

    Initial commit with project files.

---
+++ Breakout/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Breakout
+//
+//  Created by Daniel Flax on 2/2/15.
+//  Copyright (c) 2015 Daniel Flax. All rights reserved.
+//
+
+import SpriteKit
+
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let BlockNodeCategoryName = "blockNode"
+
+class GameScene: SKScene {
+	override func didMoveToView(view: SKView) {
+		super.didMoveToView(view)
+	}
+}
