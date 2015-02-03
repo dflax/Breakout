f005661e2094de44b9684260dd881ba820331d84
Daniel Flax <dflax@yahoo.com>
2015-02-03 14:01:53 -0500

    Added ball and other bits.

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -15,6 +15,20 @@ let BlockNodeCategoryName = "blockNode"
 
 class GameScene: SKScene {
 	override func didMoveToView(view: SKView) {
+
+		// Configure the Physics World itself
+		// 1. Create a physics body that borders the screen
+		let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+		// 2. Set the friction of that physicsBody to 0
+		borderBody.friction = 0
+		// 3. Set physicsBody of scene to borderBody
+		self.physicsBody = borderBody
+		physicsWorld.gravity = CGVectorMake(0, 0)
+
+		// Set up the ball with a physics body
+		let ball = childNodeWithName(BallCategoryName) as SKSpriteNode
+		ball.physicsBody!.applyImpulse(CGVectorMake(10, -10))
+
 		super.didMoveToView(view)
 	}
 }
